3eb809bb7cb48a2c8813cc9016209e87fa171a75
Leroy Steele <60223453+LeroySteele@users.noreply.github.com>
2024-09-01 22:22:06 +0200

    Update ownerSupport.js
    Updated reply process with deferReply

--- commands/utility/ownerSupport.js
+++ commands/utility/ownerSupport.js
@@ -10,8 +10,9 @@ module.exports = {
         .setDescription('Create initial message for support tickets'),
     async execute(interaction) {
         try {
-            await interaction.reply({
-                content: "Creating support message", ephemeral: true,
+            await interaction.deferReply({ ephemeral: true });
+            await interaction.editReply({
+            	content: "Creating support message", ephemeral: true,
             });
             if ( await interaction.member.roles.cache.some(role => role.id === idList.staffRole) ) {
                 if ( interaction.channelId === idList.supportmessagechannel ) {
@@ -83,6 +84,9 @@ module.exports = {
                 });
             }
         } catch (err) {
+            await interaction.editReply({
+	            content: `An error has occurred, please try again later and if the problem persists contact admin. Code: Owner Support.`, ephemeral: true,
+            });
             console.error(err);
         }
     }
