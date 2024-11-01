39744fe37f15324519bf38b414f1410e250450cb
Leroy Steele <60223453+LeroySteele@users.noreply.github.com>
2024-11-01 14:02:44 +0200

    Update buttonApplications.js

--- events/buttonApplications.js
+++ events/buttonApplications.js
@@ -1,4 +1,6 @@
-// Users can click the button to open an application ticket, the corresponding permissions will be granted to the user and a messege will be sent in that new ticket
+// When the user clicks the button a ticket (channel) will be created. First all permissions are set to false then the user and admins each get the permissions they need to interact with the channel. 
+// The ticket's name includes the user's names to show ownership and they also get tagged so it's easy to locate. A template messege is then sent to show what the ticket is about.
+// Additional button are sent for two additional templates (gang, business) and also a close button
 
 const { Events, EmbedBuilder, PermissionsBitField, ChannelType } = require('discord.js');
 const idList = require('../data/idList');
