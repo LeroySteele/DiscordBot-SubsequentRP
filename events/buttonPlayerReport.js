b9a488aa46359068ceac8df9fbe07cc478c03f1d
Leroy Steele <60223453+LeroySteele@users.noreply.github.com>
2024-11-01 13:44:34 +0200

    Update buttonPlayerReport.js

--- events/buttonPlayerReport.js
+++ events/buttonPlayerReport.js
@@ -1,3 +1,5 @@
+// Users can click the button to open a report community rule breaks in a ticket, the corresponding permissions will be granted to the user and a messege will be sent in that new ticket
+
 const { Events, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, PermissionsBitField, ChannelType } = require('discord.js');
 const idList = require('../data/idList');
 const ids = require('../data/ids');
