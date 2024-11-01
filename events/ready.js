411b1db866219752ceef14627cb2c06d1ecc2b37
Leroy Steele <60223453+LeroySteele@users.noreply.github.com>
2024-11-01 14:15:21 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const { Events, ActivityType } = require('discord.js');
 module.exports = {
     name: Events.ClientReady,
     execute(client){
-        try {
+        try { // the bots activity will be set to 'Watching over Subsequest RP'
             console.log(`Ready! Logged in as ${client.user.tag}`);
 			client.user.setPresence({
 				activities: [{ name: ` over  Subsequent RP`, type: ActivityType.Watching },]
